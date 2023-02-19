6ee8f94b008f773ca208f18c6d24d27e369bca32
MoMoAxe <tobioluwa348@gmail.com>
2023-02-19 13:39:41 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+// Online C compiler to print a line followed by standard error
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line to standard error
+ *
+ * Return: (1) (Success)
+ */
+
+int main(void) 
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
+}
