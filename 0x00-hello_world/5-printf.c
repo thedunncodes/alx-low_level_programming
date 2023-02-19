4e23bbcce26a35e51227e1a026c25228e0ab51b9
MoMoAxe <tobioluwa348@gmail.com>
2023-02-19 08:54:08 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/*C program to print an exact line*/
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
