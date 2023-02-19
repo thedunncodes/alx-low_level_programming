e1c0266783e430b4f1c175915a0a3815982843af
MoMoAxe <tobioluwa348@gmail.com>
2023-02-19 13:48:09 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  * Return: (1) (Success)
  */
 
-int main(void) 
+int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	fputs("and that piece of art is useful\" - Dora Korpar,
+		       2015-10-19\n", stdout);
 	return (1);
 }
