0a1d43a659ea5973b133dae64a49f8c2532733c6
MoMoAxe <tobioluwa348@gmail.com>
2023-02-20 12:44:54 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,14 @@
-/* This program will assign a random number to the variable n each time it is executed. */
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 
+/*./betty-doc.pl 0-positive_or_negative.c*/
 /**
  * main - Entry point
  *
- * line 17 - 18 generates a random number
- * 
- * line 20 - 22 specifies condition for the type of number
- * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	int n;
 
@@ -23,7 +18,7 @@ int main(void)
 {
 	printf("%d is negative", n);
 }
-	if (n = 0)
+	if (n == 0)
 {
 	printf("%d is zero", n);
 }
