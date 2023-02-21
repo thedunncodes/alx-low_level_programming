e05b68bf1cde56b487b1ab55115a97a66e156ad2
MoMoAxe <tobioluwa348@gmail.com>
2023-02-21 14:02:20 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,2 @@
-void hello(char name[])
-{
-	printf("%s\n", name);
-}
-
-void alphabet(char ch)
-{
-	ch;
-	for (ch ;ch <= 'z' ; ch++ )
-	putchar(ch);
-	putchar('\n');
-}
+void hello(char name[]);
+void alphabet(char ch);
