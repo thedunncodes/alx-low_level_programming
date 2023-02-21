0236c1ef6f405725a4a62b076c5a1dac0101bb1d
MoMoAxe <tobioluwa348@gmail.com>
2023-02-21 13:27:57 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Write a function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	alphabet(ch);
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,11 @@ void hello(char name[])
 {
 	printf("%s\n", name);
 }
+
+void alphabet(char ch)
+{
+	ch;
+	for (ch ;ch <= 'z' ; ch++ )
+	putchar(ch);
+	putchar('\n');
+}
