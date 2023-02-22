a2aa30fa761b013935a8da7fec956203c9dbeb36
MoMoAxe <tobioluwa348@gmail.com>
2023-02-22 09:18:24 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,6 @@
  * main - Write a program that prints _putchar, followed by a new line.
  * Description : to print _putchar
  * Return: Always 0 (Success)
- * name:  to print _putchar
  */
 int main(void)
 {
@@ -24,7 +23,5 @@ int main(void)
 
 void hello(char name[])
 {
-	char name[];
-
 	printf("%s\n", name);
 }
