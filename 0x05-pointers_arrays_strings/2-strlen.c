3eb238089fd0c2cfbf8a95f940f84467c78e3b6e
MoMoAxe <tobioluwa348@gmail.com>
2023-02-28 13:42:18 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - function to print string length
+ * @len: variable to hold strlen
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	len = strlen(s);
+}
