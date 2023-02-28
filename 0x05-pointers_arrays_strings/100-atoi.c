2ce38890508404883602123f83a6b4aabbac7b12
MoMoAxe <tobioluwa348@gmail.com>
2023-02-28 15:37:41 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,7 +4,7 @@
 /**
  * _atoi - a function that converts string to integer
  * @s: An input string
- * Return: integer from conversion
+ * Return: integer from conversion.
  */
 int _atoi(char *s)
 {
