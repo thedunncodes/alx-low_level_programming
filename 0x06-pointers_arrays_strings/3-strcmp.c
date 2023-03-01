65ae65de9f804ddca563ff17ad9bf205c1c5c034
MoMoAxe <tobioluwa348@gmail.com>
2023-03-01 12:09:55 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - to compare two strings
+ * @s1: arg1 holder
+ * @s2: arg2 holder
+ * @a: integer variable
+ * Return: a.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = strcmp(s1, s2);
+
+	return (a);
+}
