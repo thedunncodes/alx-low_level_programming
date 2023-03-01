820aecab98ed47582e7232ebddb2842041379ffb
MoMoAxe <tobioluwa348@gmail.com>
2023-03-01 07:32:52 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncat - so concatenate some characters of s2 to s1
+ * @dest: s1 holder
+ * @src: s2 holder
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+
+	return (dest);
+}
