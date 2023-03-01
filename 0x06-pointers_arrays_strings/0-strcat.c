31e44ed9ffab7fc206335bb909d307d68a7ba6d3
MoMoAxe <tobioluwa348@gmail.com>
2023-03-01 03:44:34 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - so concatenate s2 to s1
+ * @dest: s1 holder
+ * @src: s2 holder
+ * @cat: character variable
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char cat;
+
+	cat = strcat(dest, src);
+
+	/*return (dest, src);*/
+}
