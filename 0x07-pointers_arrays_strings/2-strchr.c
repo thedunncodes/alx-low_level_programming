eb066467f8d7e8bbcd7e447bf6617dc8dd40afbf
MoMoAxe <tobioluwa348@gmail.com>
2023-03-07 08:42:58 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strchr - check the code
+ * @s: where the first occurence of 'c' is returned to
+ * and and printed to the end
+ * @c: character to return to 's'
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	int j;
+	int len;
+
+	i = 0;
+	j = 0;
+	len = strlen(s);
+
+	while (i < len)
+	{
+		if ((s[i] == c) && (j == 0))
+		{
+			j = 1;
+		}
+		if (j == 1)
+		{
+			putchar(s[i]);
+		}
+			i++;
+		}
+
+		return (0);
+}
