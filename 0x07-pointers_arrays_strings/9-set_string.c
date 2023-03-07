0e0fb1d177b20dcc4db4a23ae0d4202a6584e0ec
MoMoAxe <tobioluwa348@gmail.com>
2023-03-07 14:32:35 -0800

    9-set_string.c

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -4,7 +4,7 @@
 /**
  * set_string - sets value of a pointer to a char
  * @s: double pointer to a string
- * @to: pointer to the string to set s to
+ * @to: pointer to the string to set s to.
  * Return: Description of the returned value
  */
 void set_string(char **s, char *to)
