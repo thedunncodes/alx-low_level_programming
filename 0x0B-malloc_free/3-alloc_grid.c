b125bf1ea7c3817e2df05a8cc3aa12c3c0d83479
MoMoAxe <tobioluwa348@gmail.com>
2023-03-16 04:57:24 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,6 @@ int **alloc_grid(int width, int height)
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 	{
-		free(grid);
 		return (NULL);
 	}
 
@@ -31,6 +30,10 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
+		for (j = 0; j < i; j++)
+		{
+			free(grid[j]);
+		}
 			free(grid);
 			return (NULL);
 		}
