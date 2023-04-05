1c0f0debe7f3bc2557eae2c152a60d0d44e3b415
MoMoAxe <tobioluwa348@gmail.com>
2023-04-05 03:51:49 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_node - A function that adds a node to the head of linked list
+ * @head: pointer to a pointer that points to the start of a liked list
+ * @str: pointer to a string to be added in a node
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str = (char *)str;
+	newnode->len = strlen(str);
+	newnode->next = *head;
+	*head = newnode;
+
+	return (newnode);
+}
