d2950e3cd87aa61505813e799a0a13ec0c77b845
MoMoAxe <tobioluwa348@gmail.com>
2023-12-16 03:56:39 -0800

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,7 @@
 
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int search(int *array, size_t start, size_t end, int value);
 
 #endif /*SEARCH_ALGOS_H*/
